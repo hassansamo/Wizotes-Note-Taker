612389b44ae4a8488a51d70fe933f6d42feb5cf0
Hassan Samo <hassansamo609@gmail.com>
2024-04-17 16:55:45 +0500

    initial commit
    Changed the sizing of logo

--- index.php
+++ index.php
@@ -99,7 +99,7 @@ if ($_SERVER["REQUEST_METHOD"] == 'POST') {
     <nav class="navbar navbar-expand-lg bg-body-tertiary" style="height: 60px;">
         <div class="container-fluid">
             <a class="navbar-brand" href="./index.php">
-                <img src="./logo.png" alt="Wizotes" style="height: 150px;">
+                <img src="./logo.png" alt="Wizotes" style="height: 40px; vertical-align: baseline;">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
